ab40305ed97ceb8e204e3032dff6ecfc14f314cd
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-28 14:02:00 +0200

    [add] reference terms routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :glossaries
+      resources :glossaries do
+        # resources :term, only: [:new,:create]
+        member do
+          post 'term' => 'glossaries#create_term'
+        end
+      end
+      resources :term, only: [:new,:create,:index,:show]
     end
   end
 end
