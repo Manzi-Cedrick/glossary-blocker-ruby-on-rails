96be398e2b3fabc79cd7b1f72a08ba30b667a513
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-28 19:35:14 +0200

    [add] Translation seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,14 @@ end
     Term.create({
         source_term: Faker::ProgrammingLanguage.name,
         target_term: Faker::ProgrammingLanguage.name,
-        glossary: Glossary.find(14)
+        glossary: Glossary.find(20)
+    })
+end
+5.times do 
+    Translation.create({
+        source_language_code: Faker::Book.title,
+        target_language_code: Faker::Book.title,
+        source_text: Faker::Lorem.characters,
+        glossary: Glossary.find(22)
     })
 end
